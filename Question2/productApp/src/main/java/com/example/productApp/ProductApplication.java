0e318f050e4d333701f245ccc65636b33cb1b1e0
udhojishreya <55437918+udhojishreya@users.noreply.github.com>
2025-11-21 00:39:45 +0530

    Create ProductApplication.java

---
+++ Question2/productApp/src/main/java/com/example/productApp/ProductApplication.java
@@ -0,0 +1,16 @@
+package com.example.productApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author udhoji.shreya : Product Application
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
